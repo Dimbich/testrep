7db075c3d1af72fc05b17d6bd4a5410c99f91e98
Dmitriy Shirmanov <shirmanovdmitrtiy@gmail.com>
2019-06-04 23:13:52 +0700

    Выполнил 19 урок

--- src/js/modal.js
+++ src/js/modal.js
@@ -9,4 +9,28 @@ $(document).ready(()=>{
   close.on('click', ()=>{
     modal.removeClass('modal_active');
   })
+
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.arrows__left'),
+    nextArrow: $('.arrows__right'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 })
