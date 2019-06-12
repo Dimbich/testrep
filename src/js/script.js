5117e72eb9e3fe9be8f8d7e6f2341d05272888e0
Dmitriy Shirmanov <shirmanovdmitrtiy@gmail.com>
2019-06-12 14:02:41 +0700

    Испарвил валидацию

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
-$(document).ready(()=>{
+$(document).ready(function() {
   const button = $('.navbar__button');
   const modal = $('.modal');
   const close = $('.modal-dialog__close');
-  button.on('click', ()=>{
+  button.on('click', function() {
     modal.addClass('modal_active');
   })
 
-  close.on('click', ()=>{
+  close.on('click', function() {
     modal.removeClass('modal_active');
   })
 
@@ -34,35 +34,35 @@ $(document).ready(()=>{
     ]
   });
 
-  
-
-  $("#brif-form, #offer_form").validate({
-    rules: {
-      username: {
-        required: true,
-        rangelength: [2, 15]
+  $("#brif-form,  #offer_form").each(function(){
+    $(this).validate({
+      rules: {
+        username: {
+          required: true,
+          rangelength: [2, 15]
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        phone: {
+          required: true,
+        }
       },
-      email: {
-        required: true,
-        email: true
-      },
-      phone: {
-        required: true,
-      }
-    },
-    messages: {
-      username: {
-        required:'Заполните имя',
-        rangelength: 'Введите от 2 до 15 символов'
-      },
-      email: {
-          required: "Заполните e-mail",
-          email: "Введите корректный email"
-      },
-      phone: {
-        required: "Заполните телефон",
-      }
-    }    
+      messages: {
+        username: {
+          required:'Заполните имя',
+          rangelength: 'Введите от 2 до 15 символов'
+        },
+        email: {
+            required: "Заполните e-mail",
+            email: "Введите корректный email"
+        },
+        phone: {
+          required: "Заполните телефон",
+        }
+      }    
+    });
   });
   $(".phone").mask('+7 (999) 999-99-99');
 })
